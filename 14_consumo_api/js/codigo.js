b6d97955c3104516c394e22bb28b897e75effa24
paulohenrique <pauloxzv322@gmail.com>
2025-12-16 20:57:54 -0300

    Aula 16/12/2025

--- 14_consumo_api/js/codigo.js
+++ 14_consumo_api/js/codigo.js
@@ -1,26 +1,118 @@
-const form = document.getElementById('form');
+const form = document.querySelector('form');
+const telefoneMask = document.querySelector('#telefone');
+const cepMask = document.querySelector('#cep');
+const cpfMask = document.querySelector('#cpf');
 
+
+const pesquisaCep = document.querySelector('#cep');
+
+const limpaFormularioCep = () => {
+    document.querySelector('#estado').value = ("");
+    document.querySelector('#cidade').value = ("");
+    document.querySelector('#bairro').value = ("");
+    document.querySelector('#logradouro').value = ("");
+    document.querySelector('#complemento').value = ("");
+}
+
+const meuCallback = (conteudo) => {
+    if (!("erro" in conteudo)) {
+        document.querySelector('#estado').value = (conteudo.uf);
+        document.querySelector('#cidade').value = (conteudo.localidade);
+        document.querySelector('#bairro').value = (conteudo.bairro);
+        document.querySelector('#logradouro').value = (conteudo.logradouro);
+        document.querySelector('#complemento').value = (conteudo.complemento);
+    }
+    else {
+        limpaFormularioCep();
+        alert("CEP não encontrado.");
+    }
+}
+
+
+cpfMask.addEventListener('input', function() {
+    this.value = this.value
+        .replace(/\D/g, '')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+});
+
+telefoneMask.addEventListener('input', function() {
+    this.value = this.value
+        .replace(/\D/g, '')
+        .replace(/(\d{2})(\d)/, '($1) $2')
+        .replace(/(\d{4,5})(\d)/, '$1-$2')
+        .replace(/(-\d{4})\d+?$/, '$1');
+});
+
+cepMask.addEventListener('input', function() {
+    this.value = this.value
+        .replace(/\D/g, '')
+        .replace(/(\d{5})(\d)/, '$1-$2')
+        .replace(/(-\d{3})\d+?$/, '$1');
+});
+
+pesquisaCep.addEventListener('blur' , function(){
+    let cep = document.querySelector('#cep').value.replace(/\D/g, '');
+
+    if (cep != ''){
+       let validarCep = /^[0-9]{8}$/;
+       
+       if(validarCep.test(cep)){
+       document.querySelector('#estado').value = "carregando...";
+       document.querySelector('#cidade').value = "carregando...";
+       document.querySelector('#bairro').value = "carregando...";
+       document.querySelector('#logradouro').value = "carregando...";
+       document.querySelector('#complemento').value = "carregando...";
+
+        let script = document.createElement('script');
+
+        script.src = 'https://viacep.com.br/ws/' + cep +'/json/?callback=meuCallback';
+
+        document.body.appendChild(script);
+       }
+
+    else {
+        limpaFormularioCep();
+        alert("Formato do Cep inválido.");
+    }
+    }
+    else{
+        limpaFormularioCep();
+    }
+});
 
 const exibirDados = () => {
-    let nome = document.getElementById('#nome').value;
-    let email = document.getElementById('#email').value;
-    let nascimento = document.getElementById('#nascimento').value;
-    let telefone = document.getElementById('#telefone').value;
-    let cpf = document.getElementById('#cpf').value;
-    let cep = document.getElementById('#cep').value;
-    let estado = document.getElementById('#estado').value;
-  
+    let nome = document.querySelector('#nome').value;
+    let email = document.querySelector('#email').value;
+    let nascimento = document.querySelector('#nascimento').value;
+    let telefone = document.querySelector('#telefone').value;
+    let cpf = document.querySelector('#cpf').value;
+    let cep = document.querySelector('#cep').value;
+    let estado = document.querySelector('#estado').value;
+    let cidade = document.querySelector('#cidade').value;
+    let bairro = document.querySelector('#bairro').value;
+    let logradouro = document.querySelector('#logradouro').value;
+    let complemento = document.querySelector('#complemento').value;
+    let numero = document.querySelector('#numero').value;
 
-    document.querySelector('nameresult').innerText = nome;
-    document.querySelector('emailresult').innerText = email;
-    document.querySelector('nascimentoresult').innerText = nascimento;
-    document.querySelector('telefoneresult').innerText = telefone;
-    document.querySelector('cpfresult').innerText = cpf;
-    document.querySelector('cepresult').innerText = cep;
-    document.querySelector('estadoresult').innerText = estado;
- 
-   form.requestFullscreen();
+    document.querySelector('#nomeResult').innerHTML = nome;
+    document.querySelector('#emailResult').innerHTML = `<b>E-mail:</b> ${email}`;
+    document.querySelector('#nascimentoResult').innerHTML = `<b>Data de Nascimento:</b> ${nascimento}`;
+    document.querySelector('#telefoneResult').innerHTML = `<b>Telefone:</b> ${telefone}`;
+    document.querySelector('#cpfResult').innerHTML = `<b>CPF:</b> ${cpf}`;
+    document.querySelector('#cepResult').innerHTML = `<b>CEP:</b> ${cep}`;
+    document.querySelector('#estadoResult').innerHTML = `<b>Estado:</b> ${estado}`;
+    document.querySelector('#cidadeResult').innerHTML = `<b>Cidade:</b> ${cidade}`;
+    document.querySelector('#bairroResult').innerHTML = `<b>Bairro:</b> ${bairro}`;
+    document.querySelector('#logradouroResult').innerHTML = `<b>Logradouro:</b> ${logradouro}`;
+    document.querySelector('#complementoResult').innerHTML = `<b>Complemento:</b> ${complemento}`;
+    document.querySelector('#numeroResult').innerHTML = `<b>Número:</b> ${numero}`;
+
+    form.reset();
 }
 
 form.addEventListener('submit', function(event) {
-    event.exibirDados('');});
+    event.preventDefault();
+    exibirDados();
+});
