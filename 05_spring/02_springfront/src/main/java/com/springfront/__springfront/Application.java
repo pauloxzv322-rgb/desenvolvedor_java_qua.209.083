cddff8ad2c436d97276f68826242bb3df9cf79e9
pauloxzv322 <pauloxzv322@gmail.com>
2026-01-30 19:22:46 -0300

    aula dia 29/01/2026

---
+++ 05_spring/02_springfront/src/main/java/com/springfront/__springfront/Application.java
@@ -0,0 +1,13 @@
+package com.springfront.__springfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
